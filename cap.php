8da1ca2df6dff7b291b910f6efc686fa56b90d3e
Pratchaya Chitchoosakul <pratchaya@lean-in.co>
2015-06-27 22:54:15 +0700

    Add dirty version of captcha

---
+++ cap.php
@@ -0,0 +1,380 @@
+<?php
+class cap
+{
+
+	function cap($var1, $var2, $var3, $var4)
+	{
+		if($var1 === 1)
+		{
+			if($var4 === 0 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + ZERO";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - ZERO";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * ZERO";
+				}
+			} else if($var4 === 1 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + ONE";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - ONE";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * ONE";
+				}
+			} else if($var4 === 2 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + TWO";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - TWO";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * TWO";
+				}
+			} else if($var4 === 3 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + THREE";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - THREE";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * THREE";
+				}
+			} else if($var4 === 4 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + FOUR";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - FOUR";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * FOUR";
+				}
+			} else if($var4 === 5 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + FIVE";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - FIVE";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * FIVE";
+				}
+			} else if($var4 === 6 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + SIX";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - SIX";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * SIX";
+				}
+			} else if($var4 === 7 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + SEVEN";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - SEVEN";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * SEVEN";
+				}
+			} else if($var4 === 8 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + EIGHT";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - EIGHT";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * EIGHT";
+				}
+			} else if($var4 === 9 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var2 === 0 OR $var2 === 1 OR $var2 === 2 OR $var2 === 3 OR $var2 === 4 OR $var2 === 5 OR $var2 === 6 OR $var2 === 7 OR $var2 === 8 OR $var2 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				if($var3 === 1)
+				{
+					echo "$var2 + NINE";
+				}
+
+				if($var3 === 2)
+				{
+					echo "$var2 - NINE";
+				}
+
+				if($var3 === 3)
+				{
+					echo "$var2 * NINE";
+				}
+			} else
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var4 is ' . $var4 . ', var3 is ' . $var3 . ' and var2 is ' . $var2 . "\n";
+				echo "Invalid input";
+			}
+		} else if($var1 === 2)
+		{
+			if($var2 === 0 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if(@var3 === 1)
+				{
+					echo "ZERO + $var4";
+				}
+
+				if(@var3 === 2)
+				{
+					echo "ZERO - $var4";
+				}
+
+				if(@var3 === 3)
+				{
+					echo "ZERO * $var4";
+				}
+			} else if($var2 === 1 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "ONE + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "ONE - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "ONE * $var4";
+				}
+			} else if($var2 === 2 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "TWO + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "TWO - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "TWO * $var4";
+				}
+			} else if($var2 === 3 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "THREE + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "THREE - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "THREE * $var4";
+				}
+			} else if($var2 === 4 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "FOUR + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "FOUR - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "FOUR * $var4";
+				}
+			} else if($var2 === 5 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "FIVE + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "FIVE - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "FIVE * $var4";
+				}
+			} else if($var2 === 6 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "SIX + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "SIX - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "SIX * $var4";
+				}
+			} else if($var2 === 7 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "SEVEN + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "SEVEN - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "SEVEN * $var4";
+				}
+			} else if($var2 === 8 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "EIGHT + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "EIGHT - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "EIGHT * $var4";
+				}
+			} else if($var2 === 9 AND ($var3 === 1 OR $var3 === 2 OR $var3 === 3) AND ($var4 === 0 OR $var4 === 1 OR $var4 === 2 OR $var4 === 3 OR $var4 === 4 OR $var4 === 5 OR $var4 === 6 OR $var4 === 7 OR $var4 === 8 OR $var4 === 9 ))
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				if($var3 === 1)
+				{
+					echo "NINE + $var4";
+				}
+
+				if($var3 === 2)
+				{
+					echo "NINE - $var4";
+				}
+
+				if($var3 === 3)
+				{
+					echo "NINE * $var4";
+				}
+			} else
+			{
+				echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+				echo "Invalid input";
+			}
+		}  else 
+		{
+			echo __LINE__ . " var1 is " . $var1 . ', var2 is ' . $var2 . ', var3 is ' . $var3 . ' and var4 is ' . $var4 . "\n";
+			echo "Invalid input";
+		}
+	}
+}
+
+$cap = new cap(1, 1, 1, 1);
+echo "\n\n";
+
+$cap = new cap(2, 1, 1, 1);
+echo "\n\n";
+
+$cap = new cap(3, 1, 1, 1);
+echo "\n\n";
+
+$cap = new cap(1, x, 1, 1);
+echo "\n\n";
+?>
